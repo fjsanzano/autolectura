f8ff8aa9b8f40a6870572d7709e97adf09ced62c
Fidel Jimenez Sanzano <fidel.jimenez@desoft.cu>
2020-04-22 17:36:32 -0400

    [add] add models and admin managament

---
+++ apps/lectura/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import CommercialOffice, MeterReading
+
+
+class CommercialOfficeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email','telephone')
+
+
+admin.site.register(CommercialOffice, CommercialOfficeAdmin)
+
+
+class MeterReadingAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'contract_number','commercial_office_id', 'route', 'number', 'reading')
+
+
+admin.site.register(MeterReading, MeterReadingAdmin)
+
